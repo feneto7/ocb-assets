762e2c76e4e5b137868f8caf04b2cdb384e278f6
Felipe Santiago <100135825+feneto7@users.noreply.github.com>
2023-05-08 14:34:08 -0300

    new3
    new3

---
+++ copy.bat
@@ -0,0 +1,3 @@
+robocopy clover/sprites ../clovermon-showdown-client/sprites /e
+robocopy clover/audio ../clovermon-showdown-client/audio /e
+robocopy clover-cap/sprites ../clovermon-showdown-client/sprites /e
